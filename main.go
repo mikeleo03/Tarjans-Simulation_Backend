96d36c05e38b4818c7f052867a3f973040c739e4
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-07-14 23:14:27 +0700

    refactor : port serve for deployment

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/mikeleo03/Tarjans-Algorithm_Backend/router"
 )
@@ -13,6 +14,23 @@ func main() {
 	r := router.Router()
 
     // Start server
-	fmt.Println("Starting server on the port 8080...")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	// Use `PORT` provided in environment or default to 3000
+  	var port = envPortOr("8080")
+	fmt.Println("Starting server...")
+	fmt.Println("Listening from" + port)
+	log.Fatal(http.ListenAndServe(port, r))
+	fmt.Println("The server is started!")
+}
+
+// PORT handling
+// Returns PORT from environment if found, defaults to
+// value in `port` parameter otherwise. The returned port
+// is prefixed with a `:`, e.g. `":3000"`.
+func envPortOr(port string) string {
+	// If `PORT` variable in environment exists, return it
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		return ":" + envPort
+	}
+	// Otherwise, return the value of `port` variable from function argument
+	return ":" + port
 }
